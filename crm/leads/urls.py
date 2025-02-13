89ecb6b2cd6ba37808d2c06a502703cd543f01ed
Vr3n <vickyspatel@gmail.com>
2025-02-13 11:03:10 +0530

    Organization Leads implementation.
    Models, Templates, Views, everything from my
    previous project.

---
+++ crm/leads/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+
+from leads import views
+
+urlpatterns = [
+    path('', views.all_leads_view, name="all-leads"),
+    path('<int:pk>/', views.lead_detail_view, name="lead-detail"),
+
+    # HTMX VIEWS HERE.
+    path('hx/create/', views.hx_create_lead, name="hx-create-lead"),
+    path('hx/<int:pk>/delete/', views.hx_lead_delete_view,
+         name="hx-delete-lead"),
+    path('hx/all-leads/table/', views.hx_leads_table, name="hx-leads-table"),
+
+    # HX Mobile views.
+    path('hx/mobile-numbers/table/<int:lead_id>/', views.hx_lead_mobile_table,
+         name="hx-lead-mobile-number-table"),
+    path('hx/mobile-numbers/table/create/', views.hx_mobile_table_form,
+         name="hx-leads-mobile-numbers"),
+    path('hx/mobile-numners/table/delete/<int:pk>', views.hx_lead_mobile_delete,
+         name="hx-leads-mobile-table-delete"),
+
+    # HX Email views.
+    path('hx/email-addresses/table/<int:lead_id>/', views.hx_lead_email_table,
+         name="hx-lead-email-address-table"),
+    path('hx/email-addresses/table/create/', views.hx_email_table_form,
+         name="hx-leads-email-addresses"),
+    path('hx/email-addresses/table/delete/<int:pk>/', views.hx_lead_email_delete,
+         name="hx-leads-email-table-delete"),
+
+]
